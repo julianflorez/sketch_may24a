24964cd49fb5a67e7ceab5a38e0fb453dd55fd31
julianflorez <julian.florez.sa@gmail.com>
2015-05-26 08:20:05 -0500

    Creación de Etiquetas
    Se crean etiquetas para facilitar la programación a un lenguaje más
    simple.
    Este es un intento por hacer una ayuda de programación que permita al
    programador trabajar más fácil.

--- sketch_may24a.ino
+++ sketch_may24a.ino
@@ -1,16 +1,47 @@
+//Funciones Digitales
+#define ENCENDER(pin)     digitalWrite(pin,HIGH);
+#define APAGAR(pin)       digitalWrite(pin,LOW);
+#define LEER_DIGITAL(pin) digitalRead(pin);
+#define ENTRADA           INPUT
+#define SALIDA            OUTPUT
+#define TIPO_PUERTO_DIGITAL(pin,tipo) pinMode(pin,tipo);
+#define ENCENIDIDO        1
+#define APAGADO           0
+#define IGUAL             ==
+
+//Funciones análogas
+#define LEER_ANALOGO(pin) analogRead(pin);
+
+//Función de espera o retardo
+#define ESPERA(ms)        delay(ms);
+
+//Funciones condicionales
+#define SI(condicion1,operador,valor) if(condicion1,operador,valor){
+#define SINO_HACER        else{
+#define TERMINAR_SINO     }
+#define TERMINAR_SI       }
+
+int var;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2,OUTPUT);
+  TIPO_PUERTO_DIGITAL(2,SALIDA)
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(2,HIGH);
-  delay(500);
-  digitalWrite(2,LOW);
-  delay(1000);
-  digitalWrite(2,HIGH);
-  delay(200);
-  digitalWrite(2,LOW);
-  delay(300);
+  ENCENDER(2)
+  ESPERA(500)
+  APAGAR(2)
+  ESPERA(1000)
+  ENCENDER(2)
+  ESPERA(200)
+  APAGAR(2)
+  ESPERA(300)
+  SI(LEER_DIGITAL(2),IGUAL,ENCENDIDO)
+    ESPERA(500)
+  SINO_HACER
+    ESPERA(1000)
+  TERMINAR _SINO 
+  TERMINAR_SI
+  
 }
